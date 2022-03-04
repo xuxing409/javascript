c57d4e6b17fbf47b2ed8205e3c05bc01d470f464
是柠新呀 <892573319@qq.com>
2022-03-04 21:26:39 +0800

    使用weakMap解决循环引用问题

--- 08_浅拷贝与深拷贝/index.js
+++ 08_浅拷贝与深拷贝/index.js
@@ -23,20 +23,21 @@ function deepCloneEs5(origin, target){
   }
   return tar
 }
-var objTar = {}
-deepCloneEs5(obj,objTar)
-obj.age =100;
-obj.name='Tj'
-console.log(obj, objTar);
+// var objTar = {}
+// deepCloneEs5(obj,objTar)
+// obj.age =100;
+// obj.name='Tj'
+// console.log(obj, objTar);
 
 // es6 深拷贝
 function deepClone(obj = {}) {
   // 值类型退出
-  if (typeof obj !== "obj" || obj == null) {
+  if (typeof obj !== "object" || obj == null) {
     return obj
   }
   // 声明拷贝的数组或对象
   let result;
+
   if (obj instanceof Array) {
     result = []
   } else {
@@ -53,13 +54,52 @@ function deepClone(obj = {}) {
   return result
 }
 
+
+// es6 升级版深拷贝
+function deepClonePro(obj, hashMap = new WeakMap()) {
+  // 值类型退出
+  if (typeof obj !== "object" || obj == null) {
+    return obj
+  }
+  if(obj instanceof Date){
+    return new Date(obj)
+  }
+  if(obj instanceof RegExp){
+    return new RegExp(obj)
+  }
+
+  const hashkey = hashMap.get(obj)
+
+  if(hashkey){
+    return hashkey
+  }
+
+  const target = new obj.constructor();
+  hashMap.set(obj, target)
+  for (let key in obj) {
+    // 去除原型链上继承的属性
+    if (obj.hasOwnProperty(key)) {
+      target[key] = deepClonePro(obj[key], hashMap)
+    }
+
+  }
+  return target
+}
 // console.log(obj);
 
 
-// const obj2 = deepClone(obj)
+// const obj2 = deepClonePro(obj)
 
 // obj2.name = '小野'
 // obj2.friend.name = 'xx'
 // obj2.colors[0] = 'red'
 
-// console.log(obj2);
+// console.log(obj,obj2);
+
+// 循环引用情况使用weakMap处理
+let test1 = {}
+let test2 = {}
+test2.test1 = test1
+test1.test2 = test2
+
+console.log(deepClonePro(test1));
