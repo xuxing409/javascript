f47c81841e6e5aa50a2d9b516115a2e2cd84ea5f
是柠新呀 <892573319@qq.com>
2022-03-04 20:38:36 +0800

    提交es5深拷贝

--- 08_浅拷贝与深拷贝/index.js
+++ 08_浅拷贝与深拷贝/index.js
@@ -5,6 +5,31 @@ const obj = {
   name: "哈默"
 }
 
+// es5 深拷贝
+function deepCloneEs5(origin, target){
+  var tar = target || {}
+  var toStr = Object.prototype.toString
+  var arrType = '[object Array]'
+
+  for (var key in origin) {
+    if(origin.hasOwnProperty(key)){
+      if(typeof origin[key] === 'object' && origin[key] !== null){
+        tar[key] = toStr.call(origin[key]) === arrType ? []: {}
+        deepCloneEs5(origin[key], tar[key])
+      }else{
+        tar[key] = origin[key]
+      }
+    }
+  }
+  return tar
+}
+var objTar = {}
+deepCloneEs5(obj,objTar)
+obj.age =100;
+obj.name='Tj'
+console.log(obj, objTar);
+
+// es6 深拷贝
 function deepClone(obj = {}) {
   // 值类型退出
   if (typeof obj !== "obj" || obj == null) {
@@ -28,13 +53,13 @@ function deepClone(obj = {}) {
   return result
 }
 
-console.log(obj);
+// console.log(obj);
 
 
-const obj2 = deepClone(obj)
+// const obj2 = deepClone(obj)
 
-obj2.name = '小野'
-obj2.friend.name = 'xx'
-obj2.colors[0] = 'red'
+// obj2.name = '小野'
+// obj2.friend.name = 'xx'
+// obj2.colors[0] = 'red'
 
-console.log(obj2);
+// console.log(obj2);
