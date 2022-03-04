870ed1c40e0bb5b87b16eb553f4e2f55f14f708a
xx <892573319@qq.com>
2022-03-04 13:25:41 +0800

    提交深拷贝(简化版

---
+++ 08_浅拷贝与深拷贝/index.js
@@ -0,0 +1,40 @@
+const obj = {
+  age: 20,
+  colors: ["orange", "green", "blue"],
+  friend: { name: "小夏" },
+  name: "哈默"
+}
+
+function deepClone(obj = {}) {
+  // 值类型退出
+  if (typeof obj !== "obj" || obj == null) {
+    return obj
+  }
+  // 声明拷贝的数组或对象
+  let result;
+  if (obj instanceof Array) {
+    result = []
+  } else {
+    result = {}
+  }
+
+  for (let key in obj) {
+    // 去除原型链上继承的属性
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key])
+    }
+
+  }
+  return result
+}
+
+console.log(obj);
+
+
+const obj2 = deepClone(obj)
+
+obj2.name = '小野'
+obj2.friend.name = 'xx'
+obj2.colors[0] = 'red'
+
+console.log(obj2);
