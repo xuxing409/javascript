e927a9d7437f7ade437262507b5c232dd730ccba
是柠新呀 <892573319@qq.com>
2022-03-06 09:19:36 +0800

    merge

---
+++ 10_原型原型链回顾/index.js
@@ -0,0 +1,23 @@
+/**
+ * 对应名称
+ * - prototype :原型
+ * - __proto__ : 原型链(连接点)
+ * 
+ * 从属关系
+ *  prototype -> 函数的一个属性: 对象{}
+ * __proto__ -> 对象Object的一个属性: 对象{}
+ * 对象的__proto__保存着该对象的构造函数的prototype
+ */
+
+/**
+ * Function Object: 函数  对象
+ */
+
+/**
+ * {}.hasOwnProperty用来获取当前对象拥有的属性而不是继承来的属性
+ */
+
+/**
+ * test.constructor 实例化test对象的构造函数,
+ * test.constructor 可以赋值改变构造函数 
+ */
