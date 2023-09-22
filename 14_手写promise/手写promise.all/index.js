41af8cb152cf1d7da99c13b01e8aa836786f569d
徐兴 <xuxing@hynn.com.cn>
2023-09-22 19:50:15 +0800

    feat: 新增手写promise.all函数

---
+++ 14_手写promise/手写promise.all/index.js
@@ -0,0 +1,47 @@
+function PromiseAll(promiseArray) {
+  //请在此处补全代码
+  const result = [];
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(promiseArray)) {
+      //判断一下传入的参数是否为数组
+      return reject(new Error("传入的参数不是数组！"));
+    }
+
+    promiseArray.forEach((promiseItem, index) => {
+      Promise.resolve(promiseItem)
+        .then((res) => {
+          result.push(res);
+          if (result.length === promiseArray.length) {
+            resolve(result);
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+}
+
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("p1");
+  }, 1000);
+});
+
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("p2");
+  }, 2000);
+});
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("p3");
+  }, 3000);
+});
+
+const test = PromiseAll([p1, p2, p3])
+  .then((res) => console.log(res))
+  .catch((e) => console.log(e));
+
+console.log(test);
